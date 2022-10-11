01819145689dd638e0794504b2d5db9955a92118
kravindra1415 <kravindra1415@gmail.com>
2022-10-11 13:04:04 +0530

    kendo-grid-column completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
+import { SortDescriptor } from '@progress/kendo-data-query';
+import { Observable } from 'rxjs';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'kendo-app';
+
+  public gridItems: Observable<GridDataResult> | undefined;
+  public pageSize: number = 10;
+  public skip: number = 0;
+  public sortDescriptor: SortDescriptor[] = [];
+  public filterTerm!: number;
+
+  constructor(private _productService: ProductService) {
+    this.loadGridItems();
+  }
+
+  public pageChange(event: PageChangeEvent): void {
+    this.skip = event.skip;
+    this.loadGridItems();
+  }
+
+  public handleSortChange(descriptor: SortDescriptor[]) {
+    this.sortDescriptor = descriptor;
+    this.loadGridItems();
+  }
+
+  public loadGridItems() {
+    this.gridItems = this._productService.getProducts(
+      this.skip,
+      this.pageSize,
+      this.sortDescriptor,
+      this.filterTerm);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridModule } from '@progress/kendo-angular-grid';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DateInputsModule,
+    BrowserAnimationsModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
